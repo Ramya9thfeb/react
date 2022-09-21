8bd368c623c2f7b3316ac5a26b887e91e5bbb774
Ramya <ramyagvkn@gmail.com>
2022-09-21 14:32:30 +0530

    changes with code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import GridComponent from './GridComponent';
 import Header from './Header';
+import Gridusingjson from './Gridusingjson';
+
 
 const App = () =>{
   return(
   <div>
     <Header />
+    <Gridusingjson />
       <GridComponent />
   </div>
   );
