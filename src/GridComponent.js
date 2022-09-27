278f39a985f4c3ccb82edada080b39dfe7778726
Ramya Krishnappa <RamyaKrishnappa@hudsonhomesdw.onmicrosoft.com>
2022-09-27 21:54:51 +0530

    Exporttoexcel

--- src/GridComponent.js
+++ src/GridComponent.js
@@ -1,22 +1,22 @@
-import { Grid, GridColumn } from '@progress/kendo-react-grid';
+import { Grid, GridColumn, GridToolbar } from '@progress/kendo-react-grid';
 import { process } from '@progress/kendo-data-query';
-import { useState, useEffect, useRef } from 'react';
-import { GridPDFExport } from '@progress/kendo-react-pdf';
-import { ExcelExport } from '@progress/kendo-react-excel-export';
+import {  useState,useEffect, useRef } from 'react';
+import {GridPDFExport} from '@progress/kendo-react-pdf';
+import {ExcelExport} from '@progress/kendo-react-excel-export';
+import { PDFExport } from '@progress/kendo-react-pdf';
 import index from './finance/dist/scss/index.scss';
 
 
 import axios from 'axios';
-import GridEditabledefault from './GridEditabledefault';
 
 
-const baseURL = "https://jsonplaceholder.typicode.com/todos";
-const BooleanCell = (props) => {
-  return (
-    <td>{props.dataItem[props.field] ? 'YES' : 'NO'} </td>
+const baseURL="https://jsonplaceholder.typicode.com/todos";
+const BooleanCell=(props) =>{
+  return(
+  <td>{props.dataItem[props.field] ? 'YES' : 'NO'} </td>
   )
 }
-const GridComponent = () => {
+const GridComponent=()=>{
 
   const _export = useRef(null);
 
@@ -30,59 +30,65 @@ const GridComponent = () => {
     skip: 0,
     take: 10,
   };
-  const [products, setProducts] = useState('');
+  const [products,setProducts]=useState('');
+ 
 
-
-  const getProducts = () => {
-    axios.get(baseURL)
-      .then(res => {
+ const getProducts=()=>{
+      axios.get(baseURL)
+      .then(res=>{
         setProducts(res.data)
-      }).catch(err => {
-        alert(err.message);
-      })
-  };
+      }).catch(err=>{
+    alert(err.message);
+  })
+};
 
-  useEffect(() => getProducts(), []);
+useEffect(()=>getProducts(),[ ]);
 
-  const [dataState, setDataState] = useState(products);
-  const [result, setResult] = useState(process(products, dataState));
+  const [dataState,setDataState]=useState(products);
+  const [result,setResult]=useState(process(products,dataState));
 
-
+  
   const onDataStateChange = (event) => {
     setDataState(event.dataState);
-    setResult(process(products, event.dataState));
+    setResult(process(products,event.dataState));
   }
-
+ 
   return (
     <ExcelExport data={products} ref={_export}>
-      <Grid
-
-        data={result}
-        filterable={true}
-        sortable={true}
-        onDataStateChange={onDataStateChange}
-        {...dataState}
-
-        pageable={true}
-        total={products.length}
-        groupable={true}
-
-      >
-
-
-        <GridColumn field="id" title="Product ID" editable={true} />
-        <GridColumn field="userId" title="User ID" editable={true} />
-        <GridColumn field="title" title="Description" editable={true} />
-        <GridColumn field="completed" title="Availability" cell={BooleanCell} filter='boolean' />
-
+    <Grid
+    pageable={true}
+    filterable={true}
+      sortable={true}
+      groupable={true}
+      total={products.length}
+      data={result}
+         
+      onDataStateChange={onDataStateChange}
+      {...dataState}
+      
+      
+    >
+      <GridToolbar>
+          <button
+            title="Export Excel"
+            className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary"
+            onClick={excelExport}
+          >
+            Export to Excel
+          </button>
+          
+        </GridToolbar>
         
-      </Grid>
+      
+      <GridColumn field="id" title="Product ID"   editable={true} />
+      <GridColumn field="userId" title="User ID" editable={true}/>
+      <GridColumn field="title" title="Description" editable={true} />
+      <GridColumn field="completed" title="Availability" cell={BooleanCell} filter='boolean'  />
+      
+      
+    </Grid>
     </ExcelExport>
-   
-
   );
-
 }
-
 
 export default GridComponent;
